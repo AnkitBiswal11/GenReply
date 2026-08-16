a423ffffbb28c350619d06ea1fd6a2f5c897969b
Ankit Biswal <ankit999biswal@gmail.com>
2026-08-17 01:49:17 +0530

    Update system instruction in interaction creation

--- 03_gemini_ai.py
+++ 03_gemini_ai.py
@@ -6,7 +6,7 @@ client = genai.Client(api_key="API_KEY")
 interaction = client.interactions.create(
     model="gemini-3.6-flash",
     input= command,
-    system_instruction="You are a person named Ankit. I am a chart bot.",
+    system_instruction="---------------------",
 )
 
 print(interaction.output_text)
